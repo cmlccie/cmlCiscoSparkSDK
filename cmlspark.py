f2351b988d4d84c3145b0040bdf339baa5ff59df
Chris Lunsford <chris@cmlccie.com>
2016-04-06 01:04:03 -0400

    Add return_type capability to API methods
    Enables caller to specify desired return_type to API methods.

--- cmlspark.py
+++ cmlspark.py
@@ -15,6 +15,10 @@ ROOMS_URL = 'rooms'
 MEMBERSHIPS_URL = 'memberships'
 MESSAGES_URL = 'messages'
 WEBHOOKS_URL = 'webhooks'
+GET_EXPECTED_STATUS_CODE = 200
+POST_EXPECTED_STATUS_CODE = 200
+PUT_EXPECTED_STATUS_CODE = 200
+DELETE_EXPECTED_STATUS_CODE = 204
 
 
 # Helper functions
@@ -50,219 +54,6 @@ class CMLSparkException(Exception):
 class CiscoSparkException(CMLSparkException):
     # TODO: Map Cisco Spark response codes to descriptive errors
     pass
-
-
-# Cisco Spark API methods container class
-class CiscoSparkAPI(RESTfulAPI):
-    def __init__(self, authentication_token, api_url=DEFAULT_API_URL,
-                 timeout=None):
-        super(CiscoSparkAPI, self).__init__(api_url)
-        self.authentication_token = authentication_token
-        self.request_args['timeout'] = timeout
-        self.request_args['headers'] = {
-            'Authorization': "Bearer " + self.authentication_token}
-
-    def delete(self, url, **request_args):
-        response = super(CiscoSparkAPI, self).delete(url, **request_args)
-        if response.status_code != 204:
-            response.raise_for_status()
-
-    def get_json(self, url, params=None):
-        response = self.get(url, params=params)
-        if response.status_code != 200:
-            response.raise_for_status()
-        else:
-            json_data = response.json()
-            return json_data
-
-    def get_json_items(self, url, params=None):
-        responses = self.get_iter(url, params=params)
-        for response in responses:
-            items = []
-            if response.status_code != 200:
-                response.raise_for_status()
-            else:
-                json_data = response.json()
-                if json_data.get('items'):
-                    items = json_data.get('items')
-                else:
-                    raise CMLSparkException("'items' object not found in JSON"
-                                            "data: %r" % json_data)
-            if items:
-                for item in items:
-                    yield item
-            else:
-                raise StopIteration
-
-    def post_json(self, url, json_data):
-        response = self.post(url, json=json_data)
-        if response.status_code != 200:
-            response.raise_for_status()
-        else:
-            return response.json()
-
-    def put_json(self, url, json_data):
-        response = self.put(url, json=json_data)
-        if response.status_code != 200:
-            response.raise_for_status()
-        else:
-            return response.json()
-
-    def get_people(self, email=None, displayName=None, max=None):
-        params = {}
-        if email:
-            params['email'] = email
-        elif displayName:
-            params['displayName'] = displayName
-        else:
-            raise CMLSparkException
-        if max:
-            params['max'] = max
-        people_items = self.get_json_items(PEOPLE_URL, params=params)
-        for item in people_items:
-            yield Person(item, api=self)
-
-    def get_person(self, id):
-        json_data = self.get_json(PEOPLE_URL + '/' + id)
-        return Person(json_data, api=self)
-
-    def get_person_me(self):
-        json_data = self.get_json(PEOPLE_URL + '/me')
-        return Person(json_data, api=self)
-
-    def get_rooms(self, showSipAddress=False, max=None):
-        params = {'showSipAddress': showSipAddress}
-        if max:
-            params['max'] = max
-        room_items = self.get_json_items(ROOMS_URL, params=params)
-        for item in room_items:
-            yield Room(item, api=self)
-
-    def create_room(self, title):
-        json_data = {'title': title}
-        room_item = self.post_json(ROOMS_URL, json_data)
-        return Room(room_item, api=self)
-
-    def get_room(self, id, showSipAddress=False):
-        params = {'showSipAddress': showSipAddress}
-        json_data = self.get_json(ROOMS_URL+'/'+id, params)
-        return Room(json_data, api=self)
-
-    def update_room(self, id, **kwargs):
-        assert kwargs and isinstance(kwargs, dict)
-        json_data = kwargs
-        room_item = self.put_json(ROOMS_URL+'/'+id, json_data)
-        return Room(room_item, api=self)
-
-    def delete_room(self, id):
-        self.delete(ROOMS_URL+'/'+id)
-
-    def get_memberships(self, roomId, personId=None, personEmail=None,
-                        max=None):
-        params = {'roomId': roomId}
-        if personId:
-            params['personId'] = personId
-        elif personEmail:
-            params['personEmail'] = personEmail
-        if max:
-            params['max'] = max
-        membership_items = self.get_json_items(MEMBERSHIPS_URL, params)
-        for item in membership_items:
-            yield Membership(item, api=self)
-
-    def create_membership(self, roomId, personId=None, personEmail=None,
-                          isModerator=False):
-        json_data = {'roomId': roomId, 'isModerator': isModerator}
-        if personId:
-            json_data['personId'] = personId
-        elif personEmail:
-            json_data['personEmail'] = personEmail
-        else:
-            raise CMLSparkException
-        membership_item = self.post_json(MEMBERSHIPS_URL, json_data)
-        return Membership(membership_item, api=self)
-
-    def get_membership(self, id):
-        json_data = self.get_json(MEMBERSHIPS_URL+'/'+id)
-        return Membership(json_data, api=self)
-
-    def update_membership(self, id, **kwargs):
-        assert kwargs and isinstance(kwargs, dict)
-        json_data = kwargs
-        membership_item = self.put_json(MEMBERSHIPS_URL+'/'+id, json_data)
-        return Membership(membership_item, api=self)
-
-    def delete_membership(self, id):
-        self.delete(MEMBERSHIPS_URL+'/'+id)
-
-    def get_messages(self, roomId, before=None, beforeMessage=None, max=None):
-        params = {'roomId': roomId}
-        if before:
-            params['before'] = before
-        if beforeMessage:
-            params['beforeMessage'] = beforeMessage
-        if max:
-            params['max'] = max
-        message_items = self.get_json_items(MESSAGES_URL, params)
-        for item in message_items:
-            yield Message(item, api=self)
-
-    def create_message(self, roomId=None, text=None, files=None,
-                       toPersonId=None, toPersonEmail=None):
-        json_data = {}
-        if roomId:
-            json_data['roomId'] = roomId
-        elif toPersonId:
-            json_data['toPersonId'] = toPersonId
-        elif toPersonEmail:
-            json_data['toPersonEmail'] = toPersonEmail
-        else:
-            raise CMLSparkException
-        if not text and not files:
-            raise CMLSparkException
-        if text:
-            json_data['text'] = text
-        if files:
-            json_data['files'] = files
-        message_item = self.post_json(MESSAGES_URL, json_data)
-        return Message(message_item, api=self)
-
-    def get_message(self, id):
-        json_data = self.get_json(MESSAGES_URL+'/'+id)
-        return Message(json_data, api=self)
-
-    def delete_message(self, id):
-        self.delete(MESSAGES_URL+'/'+id)
-
-    def get_webhooks(self, max=None):
-        params = {}
-        if max:
-            params['max'] = max
-        webhook_items = self.get_json_items(WEBHOOKS_URL, params)
-        for item in webhook_items:
-            yield Membership(item, api=self)
-
-    def create_webhook(self, name, targetUrl, resource, event, filter):
-        json_data = {'name': name,
-                     'targetUrl': targetUrl,
-                     'resource': resource,
-                     'event': event,
-                     'filter': filter}
-        webhook_item = self.post_json(WEBHOOKS_URL, json_data)
-        return Webhook(webhook_item, api=self)
-
-    def get_webhook(self, id):
-        webhook_item = self.get_json(WEBHOOKS_URL+'/'+id)
-        return Webhook(webhook_item, api=self)
-
-    def update_webhook(self, id, **kwargs):
-        assert kwargs and isinstance(kwargs, dict)
-        json_data = kwargs
-        webhook_item = self.put_json(WEBHOOKS_URL+'/'+id, json_data)
-        return Webhook(webhook_item, api=self)
-
-    def delete_webhook(self, id):
-        self.delete(WEBHOOKS_URL+'/'+id)
 
 
 # Cisco Spark data objects
@@ -319,3 +110,230 @@ class Webhook(SparkDataObject):
     event = None
     filter = None
     data = None
+
+
+# Cisco Spark API methods container class
+class CiscoSparkAPI(RESTfulAPI):
+    def __init__(self, authentication_token, api_url=DEFAULT_API_URL,
+                 timeout=None):
+        super(CiscoSparkAPI, self).__init__(api_url)
+        self.authentication_token = authentication_token
+        self.request_args['timeout'] = timeout
+        self.request_args['headers'] = {
+            'Authorization': "Bearer " + self.authentication_token}
+
+    def delete(self, url, **request_args):
+        response = super(CiscoSparkAPI, self).delete(url, **request_args)
+        if response.status_code != DELETE_EXPECTED_STATUS_CODE:
+            response.raise_for_status()
+
+    def _format_return(self, json_dict, return_type):
+        if issubclass(return_type, SparkDataObject):
+            return return_type(json_dict, api=self)
+        elif issubclass(return_type, JSONData):
+            return return_type(json_dict)
+        elif issubclass(return_type, dict):
+            return json_dict
+        else:
+            raise NotImplementedError("return_type: %r" % return_type)
+
+    def get_json(self, url, params=None):
+        response = self.get(url, params=params)
+        if response.status_code != GET_EXPECTED_STATUS_CODE:
+            response.raise_for_status()
+        else:
+            json_data = response.json()
+            return json_data
+
+    def get_json_items(self, url, params=None):
+        responses = self.get_iter(url, params=params)
+        for response in responses:
+            items = []
+            if response.status_code != GET_EXPECTED_STATUS_CODE:
+                response.raise_for_status()
+            else:
+                json_data = response.json()
+                if json_data.get('items'):
+                    items = json_data.get('items')
+                else:
+                    raise CMLSparkException("'items' object not found in JSON"
+                                            "data: %r" % json_data)
+            if items:
+                for item in items:
+                    yield item
+            else:
+                raise StopIteration
+
+    def post_json(self, url, json_dict):
+        response = self.post(url, json=json_dict)
+        if response.status_code != POST_EXPECTED_STATUS_CODE:
+            response.raise_for_status()
+        else:
+            return response.json()
+
+    def put_json(self, url, json_dict):
+        response = self.put(url, json=json_dict)
+        if response.status_code != PUT_EXPECTED_STATUS_CODE:
+            response.raise_for_status()
+        else:
+            return response.json()
+
+    def get_people(self, email=None, displayName=None, max=None,
+                   return_type=Person):
+        params = {}
+        if email:
+            params['email'] = email
+        elif displayName:
+            params['displayName'] = displayName
+        else:
+            raise CMLSparkException('')
+        if max:
+            params['max'] = max
+        people_items = self.get_json_items(PEOPLE_URL, params=params)
+        for item in people_items:
+            yield self._format_return(item, return_type)
+
+    def get_person(self, id, return_type=Person):
+        json_dict = self.get_json(PEOPLE_URL + '/' + id)
+        return self._format_return(json_dict, return_type)
+
+    def get_person_me(self, return_type=Person):
+        json_dict = self.get_json(PEOPLE_URL + '/me')
+        return self._format_return(json_dict, return_type)
+
+    def get_rooms(self, showSipAddress=False, max=None, return_type=Room):
+        params = {'showSipAddress': showSipAddress}
+        if max:
+            params['max'] = max
+        room_items = self.get_json_items(ROOMS_URL, params=params)
+        for item in room_items:
+            yield self._format_return(item, return_type)
+
+    def create_room(self, title, return_type=Room):
+        json_payload_dict = {'title': title}
+        json_dict = self.post_json(ROOMS_URL, json_payload_dict)
+        return self._format_return(json_dict, return_type)
+
+    def get_room(self, id, showSipAddress=False, return_type=Room):
+        params = {'showSipAddress': showSipAddress}
+        json_dict = self.get_json(ROOMS_URL+'/'+id, params)
+        return self._format_return(json_dict, return_type)
+
+    def update_room(self, id, return_type=Room, **kwargs):
+        assert kwargs
+        json_payload_dict = kwargs
+        json_dict = self.put_json(ROOMS_URL+'/'+id, json_payload_dict)
+        return self._format_return(json_dict, return_type)
+
+    def delete_room(self, id):
+        self.delete(ROOMS_URL+'/'+id)
+
+    def get_memberships(self, roomId, personId=None, personEmail=None,
+                        max=None, return_type=Membership):
+        params = {'roomId': roomId}
+        if personId:
+            params['personId'] = personId
+        elif personEmail:
+            params['personEmail'] = personEmail
+        if max:
+            params['max'] = max
+        membership_items = self.get_json_items(MEMBERSHIPS_URL, params)
+        for item in membership_items:
+            yield self._format_return(item, return_type)
+
+    def create_membership(self, roomId, personId=None, personEmail=None,
+                          isModerator=False, return_type=Membership):
+        json_payload_dict = {'roomId': roomId, 'isModerator': isModerator}
+        if personId:
+            json_payload_dict['personId'] = personId
+        elif personEmail:
+            json_payload_dict['personEmail'] = personEmail
+        else:
+            raise CMLSparkException
+        json_dict = self.post_json(MEMBERSHIPS_URL, json_payload_dict)
+        return self._format_return(json_dict, return_type)
+
+    def get_membership(self, id, return_type=Membership):
+        json_dict = self.get_json(MEMBERSHIPS_URL+'/'+id)
+        return self._format_return(json_dict, return_type)
+
+    def update_membership(self, id, return_type=Membership, **kwargs):
+        assert kwargs
+        json_payload_dict = kwargs
+        json_dict = self.put_json(MEMBERSHIPS_URL+'/'+id, json_payload_dict)
+        return self._format_return(json_dict, return_type)
+
+    def delete_membership(self, id):
+        self.delete(MEMBERSHIPS_URL+'/'+id)
+
+    def get_messages(self, roomId, before=None, beforeMessage=None, max=None,
+                     return_type=Message):
+        params = {'roomId': roomId}
+        if before:
+            params['before'] = before
+        if beforeMessage:
+            params['beforeMessage'] = beforeMessage
+        if max:
+            params['max'] = max
+        message_items = self.get_json_items(MESSAGES_URL, params)
+        for item in message_items:
+            yield self._format_return(item, return_type)
+
+    def create_message(self, roomId=None, text=None, files=None,
+                       toPersonId=None, toPersonEmail=None,
+                       return_type=Message):
+        json_payload_dict = {}
+        if roomId:
+            json_payload_dict['roomId'] = roomId
+        elif toPersonId:
+            json_payload_dict['toPersonId'] = toPersonId
+        elif toPersonEmail:
+            json_payload_dict['toPersonEmail'] = toPersonEmail
+        else:
+            raise CMLSparkException
+        if not text and not files:
+            raise CMLSparkException
+        if text:
+            json_payload_dict['text'] = text
+        if files:
+            json_payload_dict['files'] = files
+        json_dict = self.post_json(MESSAGES_URL, json_payload_dict)
+        return self._format_return(json_dict, return_type)
+
+    def get_message(self, id, return_type=Message):
+        json_dict = self.get_json(MESSAGES_URL+'/'+id)
+        return self._format_return(json_dict, return_type)
+
+    def delete_message(self, id):
+        self.delete(MESSAGES_URL+'/'+id)
+
+    def get_webhooks(self, max=None, return_type=Webhook):
+        params = {}
+        if max:
+            params['max'] = max
+        webhook_items = self.get_json_items(WEBHOOKS_URL, params)
+        for item in webhook_items:
+            yield self._format_return(item, return_type)
+
+    def create_webhook(self, name, targetUrl, resource, event, filter,
+                       return_type=Webhook):
+        json_payload_dict = {'name': name,
+                             'targetUrl': targetUrl,
+                             'resource': resource,
+                             'event': event,
+                             'filter': filter}
+        json_dict = self.post_json(WEBHOOKS_URL, json_payload_dict)
+        return self._format_return(json_dict, return_type)
+
+    def get_webhook(self, id, return_type=Webhook):
+        json_dict = self.get_json(WEBHOOKS_URL+'/'+id)
+        return self._format_return(json_dict, return_type)
+
+    def update_webhook(self, id, return_type=Webhook, **kwargs):
+        assert kwargs
+        json_payload_dict = kwargs
+        json_dict = self.put_json(WEBHOOKS_URL+'/'+id, json_payload_dict)
+        return self._format_return(json_dict, return_type)
+
+    def delete_webhook(self, id):
+        self.delete(WEBHOOKS_URL+'/'+id)
